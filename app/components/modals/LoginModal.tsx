762a71a30bcb4382f0520a6877849b96bbe449a5
Jhohanna Apus <jhohannarose.apus@msunaawan.edu.ph>
2025-11-24 22:18:11 +0800

    Implement Authentication (Log in) Frontend -APUS, JHOHANNA ROSE

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,41 +1,87 @@
-'use client';
+'use client'
 
-import Modal from './Modal';
-import { useState } from 'react';
-import useLoginModal from '@/app/hooks/useLoginModal';
-import CustomButton from '../forms/CustomButton';
+import Modal from "./Modal";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import useSignupModal from "@/app/hooks/useSignupModal";
+import CustomBtn from "../forms/CustomButton";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/action";
+import { stringify } from "querystring";
 
-const LoginModal = () => {
-    const loginModal = useLoginModal()
+const SignUpModal = () => {
+
+    //
+    // Variables
+    const router = useRouter();
+    const signupModal = useSignupModal();
+    const [email, setEmail] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [errors, setErrors] = useState<string[]>([]);
+
+    //
+    // Submit functionality
+    const submitSignup = async () => {
+        const formData = {
+            email: email,
+            password1: password1,
+            password2: password2
+        }
+
+        const response = await apiService.post('/api/auth/register/', JSON.stringify(formData));
+        if (response.access) {
+            handleLogin(response.user.pk, response.access, response.refresh);
+            signupModal.close();
+            router.push('/')
+        } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) => {
+                return error;
+            })
+            setErrors(tmpErrors);
+        }
+    }
 
     const content = (
         <>
+            <form
+                action={submitSignup}
+                className="space-y-4"
+            >
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-            <form className='space-y-4'>
-                <input type="email" placeholder='Your E-mail Address' className='w-full h-[54] px-4 border border-gray-300 rounded-xl' />
+                {errors.map((error, index) => {
+                    return (
+                        <div
+                            key={`error_${index}`}
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
 
-                <input type="password" placeholder='Your Password' className='w-full h-[54] px-4 border border-gray-300 rounded-xl' />
-            
-                <div className='p-5 bg-airbnb text-white rounded-xl opacity-80'>
-                    The error message
-                </div>
-
-                <CustomButton
-                    label="Log In"
-                    onClick={() => console.log('Test')}
+                <CustomBtn
+                    label="Submit"
+                    onClick={submitSignup}
                 />
             </form>
         </>
     )
 
     return (
-        <Modal
-            isOpen={loginModal.isOpen}
-            close={loginModal.close}
-            label="Log In"
-            content={content}
-        />
+        <>
+            <Modal
+                isOpen={signupModal.isOpen}
+                close={signupModal.close}
+                label="Sign Up"
+                content={content}
+            />
+        </>
     )
 }
 
-export default LoginModal;
+
+export default SignUpModal;

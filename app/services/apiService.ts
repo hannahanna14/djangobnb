f5515dab773219814c61e31ac35d513f0308a638
Jhohanna Apus <jhohannarose.apus@msunaawan.edu.ph>
2025-11-25 12:45:34 +0800

    Implementation Authentication Singup(bug fixing) - APUS, JHOHANNA ROSE

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -2,54 +2,42 @@ const apiService = {
     get: async function (url: string): Promise<any> {
         console.log('get', url);
 
-
         return new Promise((resolve, reject) => {
             fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
                 method: 'GET',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                    // 'Authorization': `Bearer ${token}`
                 }
             })
                 .then(response => response.json())
-                .then((json) => {
+                .then(json => {
                     console.log('Response:', json);
                     resolve(json);
                 })
-                .catch((error => {
-                    reject(error);
-                }))
-        })
+                .catch(error => reject(error));
+        });
     },
 
     post: async function (url: string, data: any): Promise<any> {
         console.log('post', url, data);
 
-
         return new Promise((resolve, reject) => {
             fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
                 method: 'POST',
-                body: data,
                 headers: {
-                    'Authorization': `Bearer`,
                     'Content-Type': 'application/json'
-                }
+                },
+                body: data
             })
                 .then(response => response.json())
-                .then((json) => {
+                .then(json => {
                     console.log('Response:', json);
-
-
-
-
                     resolve(json);
                 })
-                .catch((error => {
-                    reject(error);
-                }))
-        })
+                .catch(error => reject(error));
+        });
     },
-}
+};
 
 export default apiService;

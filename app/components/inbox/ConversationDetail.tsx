4690ac253ef0571b89642c7ad1ddcb74fc1db253
Jhohanna Apus <jhohannarose.apus@msunaawan.edu.ph>
2025-12-14 22:37:48 +0800

    Make it Possible to Connect - Apus

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -1,22 +1,56 @@
 'use client';
+
+
+import { useEffect, useState, useRef } from "react";
 import CustomButton from "@/app/components/forms/CustomButton";
+import { ConversationType } from "@/app/inbox/page";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 
 
 
-const ConversationDetail = () => {
+interface ConversationDetailProps {
+    token: string;
+    userId: string;
+    conversation: ConversationType;
+}
+
+
+const ConversationDetail: React.FC<ConversationDetailProps> = ({
+    userId,
+    token,
+    conversation
+}) => {
+
+
+    const myUser = conversation.users?.find((user) => user.id == userId)
+    const otherUser = conversation.users?.find((user) => user.id != userId)
+
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`, {
+        share: false,
+        shouldReconnect: () => true,
+    },
+    )
+
+
+    useEffect(() => {
+        console.log("Connection state changed", readyState);
+    }, [readyState]);
+
+
     return (
         <>
             <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
                 <div className="w-[80%]py-4 px-6 rounded-xl bg-gray-200">
-                    <p className="font-bold text-gray-500">Horace Vasquez</p>
+                    <p className="font-bold text-gray-500">Alice</p>
                     <p className="text-gray-500">Hi, how are you? Its been a while since we hung out.</p>
                 </div>
 
 
                 <div className="w-[80%]py-4 ml-[20%] px-6 rounded-xl bg-blue-200">
-                    <p className="font-bold text-gray-500">Jhohanna Rose O. Apus</p>
-                    <p className="text-gray-500">Well, Im good, mweheheheh. Yeah, its been a while! How have you been?</p>
+                    <p className="font-bold text-gray-500">Jhohanna Rose Apus</p>
+                    <p className="text-gray-500">Yeah, its been a while! How have you been?</p>
                 </div>
             </div>
 
--- app/inbox/[id]/page.tsx
+++ app/inbox/[id]/page.tsx
@@ -1,13 +1,47 @@
+import { getUserId } from "../../lib/action";
+import apiService from "@/app/services/apiService";
 import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+import { UserType } from "../page";
+import { getAccessToken } from "../../lib/action";
 
-const ConversationPage = () => {
+
+export type MessageType = {
+    id: string;
+    name: string;
+    body: string;
+    conversationId: string;
+    sent_to: UserType;
+    created_by: UserType
+}
+
+
+const ConversationPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const userId = await getUserId();
+    const token = await getAccessToken();
+
+
+    if (!userId || !token) {
+        return (
+            <main className="max-w-[1500px] max-auto px-6 py-12">
+                <p>You need to be authenticated...</p>
+            </main>
+        )
+    }
+
+
+    const { id } = await params;
+
+    const conversation = await apiService.get(`/api/chat/${id}/`)
+
+
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            <ConversationDetail />
-
-
-
-
+            <ConversationDetail
+                token={token}
+                userId={userId}
+                // messages={conversation.messages}
+                conversation={conversation.conversation}
+            />
         </main>
     )
 }
